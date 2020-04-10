497c3feec4030a4205838ebedb5a0980a9229b17
Vitaly Mikhailov <personal@aragas.org>
2020-04-10 15:31:40 +0300

    Init

---
+++ Core/ItemObjectPatch.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+
+using System.Reflection;
+using System.Xml;
+
+using TaleWorlds.Core;
+
+namespace Aragas.Core
+{
+    [HarmonyPatch(typeof(ItemObject))]
+    [HarmonyPatch("Deserialize")]
+    public class ItemObjectPatch
+    {
+        private static MethodInfo SetItemFlagsMethod { get; } =
+            typeof(ItemObject).GetProperty("ItemFlags").SetMethod;
+
+        public static void Postfix(ItemObject __instance, MBObjectManager objectManager, XmlNode node)
+        {
+            switch (node.Name)
+            {
+                case "CraftedItem":
+                    SetCivilian(__instance);
+                    break;
+                case "Item":
+                    if(!__instance.IsFood && !__instance.IsTradeGood && !__instance.IsAnimal)
+                        SetCivilian(__instance);
+                    break;
+            }
+
+
+        }
+
+        private static void SetCivilian(ItemObject itemObject)
+        {
+            var flags = itemObject.ItemFlags | ItemFlags.Civilian;
+            SetItemFlagsMethod.Invoke(itemObject, new object[] { flags });
+        }
+    }
+}
---
+++ MountAndBlade/CivilizedSubModule.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+
+using System;
+
+using TaleWorlds.MountAndBlade;
+
+namespace Aragas.MountAndBlade
+{
+	public class CivilizedSubModule : MBSubModuleBase
+	{
+		public CivilizedSubModule()
+		{
+			try
+			{
+				var harmony = new Harmony("org.aragas.bannerlord.civilized");
+				harmony.PatchAll(typeof(CivilizedSubModule).Assembly);
+			}
+			catch (Exception ex)
+			{
+				// TODO: Find a logger
+			}
+		}
+    }
+}
